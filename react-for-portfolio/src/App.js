610f1832b2610c3afe2b42f9b422ac056d91dc16
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-09-07 21:32:41 +0900

    ReactJS Update_CleanUp

--- react-for-portfolio/src/App.js
+++ react-for-portfolio/src/App.js
@@ -1,27 +1,20 @@
 import { useState, useEffect } from "react";
 
+function Hello(){
+  useEffect(() => {
+    console.log("created😋");
+    return () => console.log("destroyed 😑");
+  }, []);
+  return <h1>Hello</h1>
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyvalue, setKeyValue] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyValue(event.target.value);
-  useEffect(() => {
-    console.log("I run only once.");
-  },[]);
-  useEffect(() => {
-    console.log("I run when 'keyvalue' changes.");
-  }, [keyvalue]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyvalue & counter change");
-  }, [keyvalue, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input value={keyvalue} onChange={onChange} type="text" placeholder="Search here..." />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing? < Hello /> : null }
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
