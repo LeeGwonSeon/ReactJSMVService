880dcc1a39c2749710155e8592fead22b9f6a3ae
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-09-07 20:19:30 +0900

    ReactJS Update_Deps_how can use useEffect?

--- react-for-portfolio/src/App.js
+++ react-for-portfolio/src/App.js
@@ -2,13 +2,24 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyvalue, setKeyValue] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log("I run all the time");
+  const onChange = (event) => setKeyValue(event.target.value);
   useEffect(() => {
-    console.log("CALL THE API...");
+    console.log("I run only once.");
   },[]);
+  useEffect(() => {
+    console.log("I run when 'keyvalue' changes.");
+  }, [keyvalue]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyvalue & counter change");
+  }, [keyvalue, counter]);
   return (
     <div>
+      <input value={keyvalue} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
